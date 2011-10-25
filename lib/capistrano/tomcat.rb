72850a4c50cdee87253a4640b19a2d5d2ad80329
Rob Hunter <rhunter@thoughtworks.com>
2011-10-25 15:12:12 -0500

    Simple override of Capistrano's deploy:update_code

--- lib/capistrano/tomcat.rb
+++ lib/capistrano/tomcat.rb
@@ -2,6 +2,15 @@ configuration = Capistrano::Configuration.respond_to?(:instance) ?
   Capistrano::Configuration.instance(:must_exist) :
   Capistrano.configuration(:must_exist)
 
+set(:war_filename) { "#{app_name}.war" }
+
 configuration.load do
-  # what
+  namespace :deploy do
+    task :update_code, :roles => :app do
+      source_war_path = "artifacts/#{war_filename}"
+      target_war_path = File.join(current_path, war_filename)
+      top.upload source_war_path, target_war_path
+    end
+  end
 end
+
